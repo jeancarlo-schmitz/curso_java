e1e4a670a4c11eff887ff023f3473af3f9d1e46f
jeancarlo-schmitz <strolker3@gmail.com>
2022-09-07 15:39:14 -0300

    Subindo o que foi aprendido sobre criação de usuario e select em JPA

---
+++ exercicios-jpa/src/entity/Usuario.java
@@ -0,0 +1,41 @@
+package entity;
+
+import javax.persistence.Entity;
+
+import core.entity.DatabaseObject;
+
+@Entity
+public class Usuario extends DatabaseObject {
+	
+	private String nome;
+	
+	private String email;
+	
+	public Usuario() {}
+	
+	public Usuario(String nome, String email) {
+		super();
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [ID= "+ getId() + ", nome=" + nome + ", email=" + email + "]";
+	}
+	
+	
+}
---
+++ exercicios-jpa/src/teste/basico/ObterUsuarios.java
@@ -0,0 +1,38 @@
+package teste.basico;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import entity.Usuario;
+
+public class ObterUsuarios {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+//		String jpql = "SELECT u FROM Usuario u";
+//
+//		TypedQuery<Usuario> query = em.createQuery(jpql, Usuario.class);
+//		query.setMaxResults(5);
+//		
+//		List<Usuario> usuario = query.getResultList();
+		
+		List<Usuario> usuario = em
+				.createQuery("SELECT u FROM Usuario u", Usuario.class)
+				.setMaxResults(5)
+				.getResultList();
+							
+		System.out.println("\n\n");
+		usuario.stream().forEach(System.out::println);
+		
+		em.close();
+		emf.close();
+
+	}
+
+}
